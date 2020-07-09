c1237b51a4c291a7d0b14843f08c71b80f4f2ed0
chienyu <alice7089@gmail.com>
2020-07-09 10:25:06 +0800

    Updates

---
+++ precache-manifest.d54bb2d2278c9bf15a70da6fa7f8e15a.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2678816ba8b594fd4484e75f575af151",
+    "url": "/beara_practice/index.html"
+  },
+  {
+    "revision": "ba5a89995b62b526636c",
+    "url": "/beara_practice/static/css/main.c77d4b0c.chunk.css"
+  },
+  {
+    "revision": "8595de98df2c77543298",
+    "url": "/beara_practice/static/js/2.48a699ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beara_practice/static/js/2.48a699ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba5a89995b62b526636c",
+    "url": "/beara_practice/static/js/main.77afa8f4.chunk.js"
+  },
+  {
+    "revision": "f480daa7a268c9f81792",
+    "url": "/beara_practice/static/js/runtime-main.82687b91.js"
+  },
+  {
+    "revision": "640d7e4feb41f0f5db04a84cd1eca394",
+    "url": "/beara_practice/static/media/alex-black.640d7e4f.jpg"
+  },
+  {
+    "revision": "6b3ed1395ff39af5a846832cf3228fbe",
+    "url": "/beara_practice/static/media/alex-hb-brn.6b3ed139.jpg"
+  },
+  {
+    "revision": "e935cb1f0dd1ce527dd09c488b734802",
+    "url": "/beara_practice/static/media/backpacks.e935cb1f.jpg"
+  },
+  {
+    "revision": "0b17351354daa9be6a691a7348f05018",
+    "url": "/beara_practice/static/media/bracelets.0b173513.jpg"
+  },
+  {
+    "revision": "18cb2fce605221b16a7cc25cba057e84",
+    "url": "/beara_practice/static/media/carticon.18cb2fce.svg"
+  },
+  {
+    "revision": "6792ae1afb1a40cc34603a0888f33458",
+    "url": "/beara_practice/static/media/customise1.6792ae1a.jpg"
+  },
+  {
+    "revision": "ce71133a6033748e809e4aefa5fab87a",
+    "url": "/beara_practice/static/media/customise2.ce71133a.jpg"
+  },
+  {
+    "revision": "ec54c83333cd0a53c27024b4cff6e745",
+    "url": "/beara_practice/static/media/customise3.ec54c833.jpg"
+  },
+  {
+    "revision": "c27c9a0c4d5797b1e44cd76fcacaad24",
+    "url": "/beara_practice/static/media/customise4.c27c9a0c.jpg"
+  },
+  {
+    "revision": "e0c2686f2b7cb51dbb56b12949972a72",
+    "url": "/beara_practice/static/media/customise5.e0c2686f.jpg"
+  },
+  {
+    "revision": "d5518d36d83aa1ed89b8e621974b6959",
+    "url": "/beara_practice/static/media/customise6.d5518d36.jpg"
+  },
+  {
+    "revision": "0b17351354daa9be6a691a7348f05018",
+    "url": "/beara_practice/static/media/customise7.0b173513.jpg"
+  },
+  {
+    "revision": "32f5e951b27198cebd46204b3bc165af",
+    "url": "/beara_practice/static/media/customise9-1.32f5e951.jpg"
+  },
+  {
+    "revision": "dc7f54b461c65bad07f8c6da278a4801",
+    "url": "/beara_practice/static/media/customise9-2.dc7f54b4.jpg"
+  },
+  {
+    "revision": "f77c0ef95e29c0c24863f9ba571a11cf",
+    "url": "/beara_practice/static/media/customise9-3.f77c0ef9.jpg"
+  },
+  {
+    "revision": "d2e05130ed6287f0f62d7d2f96190788",
+    "url": "/beara_practice/static/media/customise9-4.d2e05130.jpg"
+  },
+  {
+    "revision": "d8cefe587ef2a1b5554b9eb36621f60e",
+    "url": "/beara_practice/static/media/customise9-5.d8cefe58.jpg"
+  },
+  {
+    "revision": "1b8fcec857b3c9df7dceeca11f1a9a56",
+    "url": "/beara_practice/static/media/customise9-6.1b8fcec8.jpg"
+  },
+  {
+    "revision": "5bc122ca915858ae46e6365008d6fee9",
+    "url": "/beara_practice/static/media/customise9-7.5bc122ca.jpg"
+  },
+  {
+    "revision": "6567c5b6f27251d6b8e20c11ef5f3dd8",
+    "url": "/beara_practice/static/media/customise9-8.6567c5b6.jpg"
+  },
+  {
+    "revision": "b55debc37eae47b1289221fa9cac4651",
+    "url": "/beara_practice/static/media/customise9-9.b55debc3.jpg"
+  },
+  {
+    "revision": "6d70f04e4f0a3cdf627189662522b1d1",
+    "url": "/beara_practice/static/media/eva-black.6d70f04e.jpg"
+  },
+  {
+    "revision": "3256d5203b632823f8ad202c91089579",
+    "url": "/beara_practice/static/media/eva-brown.3256d520.jpg"
+  },
+  {
+    "revision": "0587f70c98d05d35833d892558712ade",
+    "url": "/beara_practice/static/media/facebook.0587f70c.svg"
+  },
+  {
+    "revision": "e37cb6104627e3d175819b2ac4ef3e0f",
+    "url": "/beara_practice/static/media/frida-bp.e37cb610.jpg"
+  },
+  {
+    "revision": "d04e7965cbebd02d10112d74df459875",
+    "url": "/beara_practice/static/media/handbags.d04e7965.jpg"
+  },
+  {
+    "revision": "f195289e5d88a5a5cb1b2cfd03145c3d",
+    "url": "/beara_practice/static/media/homepagebackground.f195289e.jpg"
+  },
+  {
+    "revision": "4ea341f5d232add7d4b274a2b06e38bb",
+    "url": "/beara_practice/static/media/homepagebackground2.4ea341f5.jpg"
+  },
+  {
+    "revision": "d13450f4e3632982433977608d67b085",
+    "url": "/beara_practice/static/media/homepagebackground3.d13450f4.jpg"
+  },
+  {
+    "revision": "60b0488547159a0e701cf0edaedeacf8",
+    "url": "/beara_practice/static/media/instagram.60b04885.svg"
+  },
+  {
+    "revision": "9fa778638a3984793ade9afda5d6fd30",
+    "url": "/beara_practice/static/media/lila.9fa77863.jpg"
+  },
+  {
+    "revision": "0e5d269f463ee42c8c199e46e37178b7",
+    "url": "/beara_practice/static/media/logo.0e5d269f.svg"
+  },
+  {
+    "revision": "7e8d63c62ee1b85621bfa7bcdde3f46c",
+    "url": "/beara_practice/static/media/london1.7e8d63c6.jpg"
+  },
+  {
+    "revision": "2081e5082dcc979de7939911d9ab8bee",
+    "url": "/beara_practice/static/media/london2.2081e508.jpg"
+  },
+  {
+    "revision": "d320d5a518fcd85f5cbd285e1a5f5c83",
+    "url": "/beara_practice/static/media/loveandhope.d320d5a5.jpg"
+  },
+  {
+    "revision": "fe8fe18ac7ad10141c4eabf038f903c5",
+    "url": "/beara_practice/static/media/ourstory.fe8fe18a.jpg"
+  },
+  {
+    "revision": "f03f17da3a03f9f662bccf8287d740e6",
+    "url": "/beara_practice/static/media/ourstory2.f03f17da.jpg"
+  },
+  {
+    "revision": "0b048e6ff7d885070b351b38e0799c55",
+    "url": "/beara_practice/static/media/ourstory3.0b048e6f.jpg"
+  },
+  {
+    "revision": "cc87a0ef4c51a980d459c729a36f864e",
+    "url": "/beara_practice/static/media/phoenix.cc87a0ef.jpg"
+  },
+  {
+    "revision": "77d9ab1900762d55b2074d83896ea81f",
+    "url": "/beara_practice/static/media/pinterest.77d9ab19.svg"
+  },
+  {
+    "revision": "2fcd74559188f8209eb406c6784962b7",
+    "url": "/beara_practice/static/media/santa-anna-m.2fcd7455.jpg"
+  },
+  {
+    "revision": "34f5b67a1c08b3206c3fb1fd3914026a",
+    "url": "/beara_practice/static/media/santa-anna-s.34f5b67a.jpg"
+  },
+  {
+    "revision": "ab4627555835214c1e5ad4da7fb9a1f2",
+    "url": "/beara_practice/static/media/santa-anna-xs.ab462755.jpg"
+  },
+  {
+    "revision": "79670803d353bda24c8a43b3ed9a674c",
+    "url": "/beara_practice/static/media/shanghai1.79670803.jpg"
+  },
+  {
+    "revision": "b07d3d83a8ddb8cbc989514addfef84b",
+    "url": "/beara_practice/static/media/shanghai2.b07d3d83.jpg"
+  },
+  {
+    "revision": "223a7612dd7bb9d7ac4cbe4098ff0926",
+    "url": "/beara_practice/static/media/shanghai3.223a7612.jpg"
+  },
+  {
+    "revision": "8bfa18a614749066fed760a56ab872a4",
+    "url": "/beara_practice/static/media/shanghai4.8bfa18a6.jpg"
+  },
+  {
+    "revision": "f1aeacd63d7f1f82b1fb6b05b902e507",
+    "url": "/beara_practice/static/media/shanghai5.f1aeacd6.jpg"
+  },
+  {
+    "revision": "41b95b2761c7528399e025647e2ad403",
+    "url": "/beara_practice/static/media/shanghai6.41b95b27.jpg"
+  },
+  {
+    "revision": "fb6be15c361eb8f3b36b9eeb1fe2a825",
+    "url": "/beara_practice/static/media/shanghai7.fb6be15c.jpg"
+  },
+  {
+    "revision": "66e2ed70493c5e9559d989f96f19ff00",
+    "url": "/beara_practice/static/media/stachels.66e2ed70.jpg"
+  },
+  {
+    "revision": "034a10eb3ee051bdaa8d59891865271f",
+    "url": "/beara_practice/static/media/summersale.034a10eb.jpg"
+  },
+  {
+    "revision": "193592504843789d7c32c0386a2abb5e",
+    "url": "/beara_practice/static/media/weibo.19359250.svg"
+  },
+  {
+    "revision": "b960e519af8d56ea761c29bbbc4ed207",
+    "url": "/beara_practice/static/media/white-penny.b960e519.gif"
+  },
+  {
+    "revision": "13b982797116727c66770a18b6b7efd4",
+    "url": "/beara_practice/static/media/will-satchel-l.13b98279.jpg"
+  },
+  {
+    "revision": "6bbbbeb70c70e7130e32a88b6ae1e0cf",
+    "url": "/beara_practice/static/media/will-satchel-m.6bbbbeb7.jpg"
+  },
+  {
+    "revision": "ba692daf62c903077246a47445677f85",
+    "url": "/beara_practice/static/media/will-satchel-s.ba692daf.jpg"
+  },
+  {
+    "revision": "33b52a48930f0fb8ca61cb1ce4fc876e",
+    "url": "/beara_practice/static/media/wrapping-paper.33b52a48.jpg"
+  },
+  {
+    "revision": "9dabd21214407232025bc8d81eb8ca17",
+    "url": "/beara_practice/static/media/yoyo-black.9dabd212.jpg"
+  },
+  {
+    "revision": "7e74cc7f7de3b7b2ff94b81b5f94aa95",
+    "url": "/beara_practice/static/media/yoyo-brown.7e74cc7f.jpg"
+  }
+]);
