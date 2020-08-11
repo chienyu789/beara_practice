be5fb55a1be08c2514032d1c0fce5b1c7184f3d9
chienyu <alice7089@gmail.com>
2020-08-11 16:51:48 +0800

    repair imgUrl

--- src/components/collection/collections.components.jsx
+++ src/components/collection/collections.components.jsx
@@ -13,7 +13,7 @@ const CollectionPage = ({ collections }) => (
       collections.map(({ items }) => items.map((item) => (
         <div key={item.id} className="collectionitem">
           <Link to={process.env.PUBLIC_URL + item.linkUrl}>
-            <img src={item.imgUrl} alt={item.name} />
+            <img src={process.env.PUBLIC_URL + item.imgUrl} alt={item.name} />
           </Link>
           <span>{item.name}</span>
           <span>
